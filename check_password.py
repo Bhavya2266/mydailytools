d8c9f8f22a088851b729844ae13f81f4988d0f33
akc123000 <akc729147@gmail.com>
2024-04-24 16:49:27 +0545

    new modification

--- check_password.py
+++ check_password.py
@@ -1,4 +1,4 @@
-import random
+"""import random
 import string
 
 def generate_password(letters,upper case, lower case,numbers,special_chars):
@@ -8,6 +8,21 @@ if letters=='both':
 elif letters == 'lower':
    characters += string.ascii_letters
 elif letters== 'upper':
-    characters += string.ascii_letters
+    characters += string.ascii_letters """
     
 
+import string
+import random
+
+def generate_password(length):
+    """Generate a random password of the given length."""
+    # Define the set of possible characters
+    chars = string.ascii_letters + string.digits + string.punctuation
+    
+    # Generate a random password by selecting characters from the set
+    password = ''.join(random.choice(chars) for i in range(length))
+    
+    return password
+
+# Example usage:
+print(generate_password(12))
